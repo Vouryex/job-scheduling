2d0202b70477a93a69666b36b6fc3f9161b2253b
Gamaliel Petralba <petralbagamaliel@gmail.com>
2018-10-21 22:05:10 +0800

    implement srpt and priority

--- job-scheduling.py
+++ job-scheduling.py
@@ -1,4 +1,5 @@
 import copy
+from operator import attrgetter
 
 
 class Process:
@@ -52,22 +53,121 @@ def sjf(processes):
     display_processes(processes_sjf, "SJF")
 
 
+def get_arrivals(processes):
+    arrivals = [processes[0]]
+    arrival = processes[0]
+
+    for i in range(1, len(processes)):
+        if arrival.arrival == processes[i].arrival:
+            arrivals.append(processes[i])
+        else:
+            break
+    return arrivals
+
+
+def to_execute(processes_queue):
+    options = [processes_queue[0]]
+    first_option = processes_queue[0]
+    for process in processes_queue:
+        if process.burst_time == first_option.burst_time:
+            options.append(process)
+        else:
+            break
+    return min(options, key=attrgetter('arrival'))
+
+
+def restore_burst_time(processes_srpt_output, processes):
+    for i in range(0, len(processes)):
+        processes_srpt_output[i].burst_time = processes[i].burst_time
+
+    return processes_srpt_output
+
+
 def srpt(processes):
-    pass
-    # time = 0
-    # processes_srpt = copy.deepcopy(processes)
-    # processes_srpt.sort(key=lambda process: process.arrival)
-    # display_processes(processes_srpt, "SRPT")
+    time = 0
+    processes_srpt_output = []
+    processes_queue = []
+    processes_srpt = copy.deepcopy(processes)
+    processes_srpt.sort(key=lambda process: process.arrival)
+    while True:
+        arrivals = get_arrivals(processes_srpt)
+        processes_srpt = processes_srpt[len(arrivals):]
+        processes_queue.extend(arrivals)
+        processes_queue.sort(key=lambda process: process.burst_time)
+        process_to_execute = to_execute(processes_queue)
+
+        if len(processes_srpt) == 0:
+            break
+
+        next_arrival = processes_srpt[0]
+        if process_to_execute.burst_time <= next_arrival.arrival - time:
+            process_to_execute.waiting_time += time - process_to_execute.arrival
+            time += process_to_execute.burst_time
+            process_to_execute.turnaround_time = time
+            processes_queue.remove(process_to_execute)
+            processes_srpt_output.append(process_to_execute)
+        else:
+            process_to_execute.burst_time -= (next_arrival.arrival - time)
+            process_to_execute.waiting_time += (time - next_arrival.arrival)
+            time = next_arrival.arrival
+
+    for process in processes_queue:
+        process.waiting_time += time - process_to_execute.arrival
+        time += process.burst_time
+        process.turnaround_time = time
+        processes_srpt_output.append(process)
+
+    processes_srpt_output.sort(key=lambda process: process.label)
+    processes_srpt_output = restore_burst_time(processes_srpt_output, processes)
+    display_processes(processes_srpt_output, "SRPT")
+
+
+def best_priority(processes):
+    best = processes[0]
+    for i in range(1, len(processes)):
+        process = processes[i]
+        if process.priority == best.priority:
+            if process.label < best.label:
+                best = process
+        else:
+            break
+    return best
+
+
+def priority(processes):
+    time = 0
+    processes_priority_out = []
+    processes_priority = copy.deepcopy(processes)
+    processes_priority.sort(key=lambda process: process.priority)
+    while len(processes_priority) != 0:
+        process = best_priority(processes_priority)
+        process.waiting_time = time
+        time += process.burst_time
+        process.turnaround_time = time
+        processes_priority.remove(process)
+        processes_priority_out.append(process)
+    processes_priority_out.sort(key=lambda process: process.label)
+    display_processes(processes_priority_out, "Priority")
+
+
+def awt(processes):
+    return sum(process.waiting_time for process in processes) / len(processes)
+
+
+def att(processes):
+    return sum(process.turnaround_time for process in processes) / len(processes)
 
 
 def display_processes(processes, scheduling):
     print(scheduling)
     for process in processes:
         print(process)
-    print()
+    print("Average waiting time: {}".format(awt(processes)))
+    print("Average turnaround time: {}".format(att(processes)), end="\n\n")
 
 
 processes = init_processes('process1.txt')
 fcfs(processes)
 sjf(processes)
 srpt(processes)
+priority(processes)
